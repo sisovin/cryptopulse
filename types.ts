ed6927be24a2b5df7aa67e618d646c0d0a0e9043
Your Name <you@example.com>
2025-11-16 15:37:19 +0700

    Add remaining assets, vscode settings and update app/package files

--- types.ts
+++ types.ts
@@ -6,8 +6,12 @@ export interface CryptoCurrency {
   image: string;
   current_price: number;
   market_cap: number;
+  market_cap_rank: number;
   total_volume: number;
+  high_24h: number;
   price_change_percentage_24h: number;
+  circulating_supply: number;
+  ath: number;
 }
 
 export interface PortfolioAsset {
